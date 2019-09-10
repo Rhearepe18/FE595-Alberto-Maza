439734a0183beba9bcfe7d1df45aa98b8c8a33f7
Rhearepe18 <54646480+Rhearepe18@users.noreply.github.com>
2019-09-10 18:39:09 -0400

    add tan function
    added tan function 3

--- PythonRefresher_AlbertoMaza.py
+++ PythonRefresher_AlbertoMaza.py
@@ -31,3 +31,16 @@ plt.legend(['sin(x)','cos(x)'])
 
 plt.show()
 
+
+###Add tan function
+
+function3 = np.tan(x)
+plt.plot(x,function1,x,function2, x, function3)
+plt.xlabel('0 to 2*pi')
+
+plt.ylabel('sin(x), cos(x) & tan(x)')
+
+plt.title('Sin(x), Cos(x) and Tan(x) Plot')
+
+plt.legend(['sin(x)','cos(x)','tan(x)'])
+plt.show()
